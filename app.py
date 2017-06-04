f2b59a50fae61b9b55e397f2dc7861b9383e2a5e
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-06-05 00:26:44 +0530

    codechef scraping bug fixed

--- app.py
+++ app.py
@@ -20,26 +20,41 @@ result = []
 resultSet = {"present_contests":[],"upcoming_contests":[]}
 tableList = soup.findAll("table",attrs = {"class":"dataTable"})  #listofAllLinks
 
-present_contests = tableList[0].findAll("tr")  #present contests
-for contest in present_contests[1:]:
-    event_details_row = contest.findAll("td")
-    code = event_details_row[0].string
-    name = event_details_row[1].string
-    start_time = event_details_row[2].string
-    end_time = event_details_row[3].string
-    contest_url = "www.codechef.com/" + code
-    resultSet["present_contests"].append({"code":code,"platform":"codechef","name":name,"start_time":start_time,"end_time":end_time,"contest_url":contest_url})
+contestTypes = soup.findAll("h3");
 
-upcoming_contests = tableList[1].findAll("tr")  #upcoming contests
-for contest in upcoming_contests[1:]:
-    event_details_row = contest.findAll("td")
-    code = event_details_row[0].string
-    name = event_details_row[1].string
-    start_time = event_details_row[2].string
-    end_time = event_details_row[3].string
-    contest_url = "www.codechef.com/" + code
-    resultSet["upcoming_contests"].append({"code":code,"platform":"codechef","name":name,"start_time":start_time,"end_time":end_time,"contest_url":contest_url})
+i = 0
+present_contests = []
+upcoming_contests = []
+for contest in contestTypes:
+    if(contest.get_text() == 'Present Contests'):
+        present_contests = tableList[i].findAll("tr")
+    elif(contest.get_text() == 'Future Contests'):
+        upcoming_contests = tableList[i].findAll("tr")
+    i = i+1
 
+
+#present_contests = tableList[0].findAll("tr")  #present contests
+if(len(present_contests) > 0 ):
+    for contest in present_contests[1:]:
+        event_details_row = contest.findAll("td")
+        code = event_details_row[0].string
+        name = event_details_row[1].string
+        start_time = event_details_row[2].string
+        end_time = event_details_row[3].string
+        contest_url = "www.codechef.com/" + code
+        resultSet["present_contests"].append({"code":code,"platform":"codechef","name":name,"start_time":start_time,"end_time":end_time,"contest_url":contest_url})
+
+
+#upcoming_contests = tableList[1].findAll("tr")  #upcoming contests
+if(len(upcoming_contests) > 0 ):
+    for contest in upcoming_contests[1:]:
+        event_details_row = contest.findAll("td")
+        code = event_details_row[0].string
+        name = event_details_row[1].string
+        start_time = event_details_row[2].string
+        end_time = event_details_row[3].string
+        contest_url = "www.codechef.com/" + code
+        resultSet["upcoming_contests"].append({"code":code,"platform":"codechef","name":name,"start_time":start_time,"end_time":end_time,"contest_url":contest_url})
     
 '''
 adding codeforces events without scraping using Codeforces api
